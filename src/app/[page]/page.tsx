db439664e09e8df17b1bfffcf202c000952b3e43
gamzenurtukel <gamzenurtukel@gmail.com>
2023-12-18 01:35:58 +0300

    button disabled added

--- src/app/[page]/page.tsx
+++ src/app/[page]/page.tsx
@@ -102,7 +102,10 @@ const page = () => {
             <li>
               <button
                 className="flex items-center justify-center px-4 h-14 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                onClick={() => handlePage(currentPage - 1)}
+                disabled={totalPages == 1}
+                onClick={() => {
+                  handlePage(currentPage - 1);
+                }}
               >
                 Previous
               </button>
@@ -138,6 +141,7 @@ const page = () => {
               <button
                 className="flex items-center justify-center px-4 h-14 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                 onClick={() => handlePage(currentPage + 1)}
+                disabled={currentPage == totalPages}
               >
                 Next
               </button>
