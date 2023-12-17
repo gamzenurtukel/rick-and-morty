b963308661dea1c6a9aab3f67afcba8c13f0cba4
gamzenurtukel <gamzenurtukel@gmail.com>
2023-12-17 17:43:58 +0300

    fixed

--- src/app/[page]/page.tsx
+++ src/app/[page]/page.tsx
@@ -43,56 +43,58 @@ const page = () => {
         </div>
       )}
       {pathname == "/location" && <TableList data={data} />}
-      <nav
-        aria-label="Page navigation example"
-        className=" flex items-center justify-end gap-5 p-5 pb-10 dark:bg-gray-900 bg-white"
-      >
-        <ul className="inline-flex -space-x-px text-base h-10">
-          <li>
-            <button
-              className="flex items-center justify-center px-4 h-14 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-              onClick={() => handlePage(currentPage - 1)}
-            >
-              Previous
-            </button>
-          </li>
-          {Array.from(Array(totalPages).keys()).map((page) => {
-            const startPage = Math.max(
-              1,
-              currentPage - Math.floor(pageRange / 2)
-            );
-            const endPage = Math.min(totalPages, startPage + pageRange - 1);
-
-            if (page + 1 >= startPage && page + 1 <= endPage) {
-              return (
-                <li key={page}>
-                  <button
-                    onClick={() => handlePage(page + 1)}
-                    className={`flex items-center justify-center px-4 h-14 leading-tight text-gray-500 bg-white border border-gray-300 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 ${
-                      currentPage == page + 1
-                        ? "text-blue-600 border border-gray-300 bg-gray-300 dark:border-gray-600 dark:bg-gray-600  dark:text-white"
-                        : " hover:bg-gray-100 hover:text-gray-700  dark:hover:bg-gray-700 dark:hover:text-white"
-                    }`}
-                  >
-                    {page + 1}
-                  </button>
-                </li>
+      {data.length > 0 && (
+        <nav
+          aria-label="Page navigation example"
+          className=" flex items-center justify-end gap-5 p-5 pb-10 dark:bg-gray-900 bg-white"
+        >
+          <ul className="inline-flex -space-x-px text-base h-10">
+            <li>
+              <button
+                className="flex items-center justify-center px-4 h-14 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                onClick={() => handlePage(currentPage - 1)}
+              >
+                Previous
+              </button>
+            </li>
+            {Array.from(Array(totalPages).keys()).map((page) => {
+              const startPage = Math.max(
+                1,
+                currentPage - Math.floor(pageRange / 2)
               );
-            }
+              const endPage = Math.min(totalPages, startPage + pageRange - 1);
 
-            return null;
-          })}
+              if (page + 1 >= startPage && page + 1 <= endPage) {
+                return (
+                  <li key={page}>
+                    <button
+                      onClick={() => handlePage(page + 1)}
+                      className={`flex items-center justify-center px-4 h-14 leading-tight text-gray-500 bg-white border border-gray-300 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 ${
+                        currentPage == page + 1
+                          ? "text-blue-600 border border-gray-300 bg-gray-300 dark:border-gray-600 dark:bg-gray-600  dark:text-white"
+                          : " hover:bg-gray-100 hover:text-gray-700  dark:hover:bg-gray-700 dark:hover:text-white"
+                      }`}
+                    >
+                      {page + 1}
+                    </button>
+                  </li>
+                );
+              }
 
-          <li>
-            <button
-              className="flex items-center justify-center px-4 h-14 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-              onClick={() => handlePage(currentPage + 1)}
-            >
-              Next
-            </button>
-          </li>
-        </ul>
-      </nav>
+              return null;
+            })}
+
+            <li>
+              <button
+                className="flex items-center justify-center px-4 h-14 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                onClick={() => handlePage(currentPage + 1)}
+              >
+                Next
+              </button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </Fragment>
   );
 };
