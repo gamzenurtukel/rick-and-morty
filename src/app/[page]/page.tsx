82cdc78c867ebc15e095091ce488d3df3c86ccb6
gamzenurtukel <gamzenurtukel@gmail.com>
2023-12-17 05:42:44 +0300

    fixed

--- src/app/[page]/page.tsx
+++ src/app/[page]/page.tsx
@@ -63,10 +63,10 @@ const page = () => {
                 <li key={page}>
                   <button
                     onClick={() => handlePage(page + 1)}
-                    className={`flex items-center justify-center px-4 h-14 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
+                    className={`flex items-center justify-center px-4 h-14 leading-tight text-gray-500 bg-white border border-gray-300 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 ${
                       currentPage == page + 1
-                        ? "text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-600 dark:bg-gray-600 dark:text-white"
-                        : ""
+                        ? "text-blue-600 border border-gray-300 bg-gray-300 dark:border-gray-600 dark:bg-gray-600  dark:text-white"
+                        : " hover:bg-gray-100 hover:text-gray-700  dark:hover:bg-gray-700 dark:hover:text-white"
                     }`}
                   >
                     {page + 1}
