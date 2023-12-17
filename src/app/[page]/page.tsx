6f05dee2313ea4e0e51ef4bd7d90d143d7b4c49e
gamzenurtukel <gamzenurtukel@gmail.com>
2023-12-17 23:24:46 +0300

    fixed

--- src/app/[page]/page.tsx
+++ src/app/[page]/page.tsx
@@ -87,7 +87,7 @@ const page = () => {
                       onClick={() => handlePage(page + 1)}
                       className={`flex items-center justify-center px-4 h-14 leading-tight text-gray-500 bg-white border border-gray-300 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 ${
                         currentPage == page + 1
-                          ? "text-blue-600 border border-gray-300 bg-gray-300 dark:border-gray-600 dark:bg-gray-600  dark:text-white"
+                          ? "text-blue-600 border border-gray-300 bg-gray-400 dark:border-gray-700 dark:bg-gray-500  dark:text-white"
                           : " hover:bg-gray-100 hover:text-gray-700  dark:hover:bg-gray-700 dark:hover:text-white"
                       }`}
                     >
