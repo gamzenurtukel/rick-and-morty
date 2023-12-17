bc0a64e52eb20c8e5fc18a2c82e7d75806cf62bd
gamzenurtukel <gamzenurtukel@gmail.com>
2023-12-17 22:20:05 +0300

    search functions added

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,22 +1,54 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import ThemeConfig from "./ThemeConfig";
 import CharacterFavorite from "./CharacterFavorite";
-// import { SearchIcon } from "@heroicons/react/outline";
+import { addSearch } from "../redux/features/serachSlice";
+import { useDispatch } from "react-redux";
 
 const Header: React.FC = () => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const dispatch = useDispatch();
+
   return (
     <div className="flex items-center gap-7 h-30 p-5 dark:bg-gray-800  bg-white  shadow-lg shadow-gray-300/50  dark:shadow-gray-600/50 ">
       <div className="bg-gray-600 rounded-lg p-3 text-2xl font-bold text-white ">
         Rick And Morty
       </div>
-      <div className="flex flex-1 items-center gap-2 border dark:bg-gray-600 bg-white  p-3 rounded-lg">
-        <input
-          type="text"
-          className="flex-1 bg-transparent outline-none "
-          placeholder="Search"
-        />
-        {/* <SearchIcon className="w-6 h-6 text-purple-600 mr-1" /> */}
+
+      <div className="flex flex-1 ">
+        <div className="flex-1  border dark:border-gray-700 dark:bg-gray-600 bg-white  p-3 rounded-l-lg">
+          <input
+            type="text"
+            className="flex-1 bg-transparent outline-none "
+            placeholder="Search"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+        </div>
+        <button
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700  rounded-r-lg hover:bg-blue-800  dark:bg-blue-600 dark:hover:bg-blue-700"
+          onClick={() => {
+            dispatch(addSearch(searchValue));
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </button>
       </div>
+
       <ThemeConfig />
       <CharacterFavorite />
     </div>
