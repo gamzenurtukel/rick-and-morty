3a2a3a2c93a5d402fa70030131039a37c71745ff
gamzenurtukel <gamzenurtukel@gmail.com>
2023-12-17 05:33:16 +0300

    handleAddFavorite function added

--- src/app/components/CardItem.tsx
+++ src/app/components/CardItem.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React from "react";
 import Image from "next/image";
+import { useDispatch } from "react-redux";
+import { addCharacterFavorite } from "../redux/features/characterFavoriteSlice";
 
 interface Props {
   item: {
@@ -12,15 +14,17 @@ interface Props {
 }
 
 const CardItem: React.FC<Props> = ({ item }) => {
+  const dispatch = useDispatch();
+
+  const handleAddFavorite = (data: any) => {
+    dispatch(addCharacterFavorite(data));
+  };
   return (
     <div
-      key={item.id}
+      key={item?.id}
       className=" bg-white border border-gray-200 rounded-lg shadow
           dark:bg-gray-800 dark:border-gray-700 w-50"
     >
-      {/* <a href="#">
-        <img className="rounded-t-lg" src={item.image} alt="" />
-      </a> */}
       <div>
         <Image
           src={item.image}
@@ -40,27 +44,14 @@ const CardItem: React.FC<Props> = ({ item }) => {
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
           {item.species}
         </p>
-        <a
-          href="#"
+        <button
           className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          onClick={() => {
+            handleAddFavorite(item);
+          }}
         >
-          Read more
-          {/* <svg
-                className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg> */}
-        </a>
+          Add Favorite
+        </button>
       </div>
     </div>
   );
