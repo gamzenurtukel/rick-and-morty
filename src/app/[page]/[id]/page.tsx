aac86bc93632aa6ffeb9029d7c84febd1ee7e7ca
gamzenurtukel <gamzenurtukel@gmail.com>
2023-12-17 18:14:19 +0300

    LoadingSpinner added

--- src/app/[page]/[id]/page.tsx
+++ src/app/[page]/[id]/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 import BaseUrl from "@/app/services/BaseUrl";
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import Image from "next/image";
+import LoadingSpinner from "@/app/components/LoadingSpinner";
 
 interface PageProps {
   params: {
@@ -35,42 +36,48 @@ const Page: React.FC<PageProps> = ({ params }) => {
   }, [params.id]);
 
   return (
-    <div>
-      <div className="mx-10 dark:bg-gray-900 bg-white pt-7">
-        <div
-          key={data?.id}
-          className=" bg-white border border-gray-200 rounded-lg shadow
+    <Fragment>
+      {data ? (
+        <div>
+          <div className="mx-10 dark:bg-gray-900 bg-white pt-7">
+            <div
+              key={data?.id}
+              className=" bg-white border border-gray-200 rounded-lg shadow
                     dark:bg-gray-800 dark:border-gray-700 w-50"
-        >
-          <div className="flex flex-row">
-            <div className="flex-1">
-              <Image
-                src={data?.image || ""}
-                alt=""
-                width={700}
-                height={700}
-                className="rounded-t-lg w-full"
-              />
-            </div>
-            <div className="p-5 flex-1 ">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
-                {data?.name}
-              </h5>
+            >
+              <div className="flex flex-row">
+                <div className="flex-1">
+                  <Image
+                    src={data?.image || ""}
+                    alt=""
+                    width={700}
+                    height={700}
+                    className="rounded-t-lg w-full"
+                  />
+                </div>
+                <div className="p-5 flex-1 ">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
+                    {data?.name}
+                  </h5>
 
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                {data?.species}
-              </p>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                {data?.gender}
-              </p>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                {data?.status}
-              </p>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {data?.species}
+                  </p>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {data?.gender}
+                  </p>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {data?.status}
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <LoadingSpinner />
+      )}
+    </Fragment>
   );
 };
 
